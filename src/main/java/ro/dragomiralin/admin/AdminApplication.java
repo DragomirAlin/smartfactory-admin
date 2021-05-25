90cab3c17649659e0399d4c9ca0b4f948a04c873
dragomiralin <dragomirdanielalin@yahoo.com>
2021-05-25 21:58:05 +0300

    fix security

--- src/main/java/ro/dragomiralin/admin/AdminApplication.java
+++ src/main/java/ro/dragomiralin/admin/AdminApplication.java
@@ -1,16 +1,15 @@
 package ro.dragomiralin.admin;
 
+import de.codecentric.boot.admin.server.config.AdminServerHazelcastAutoConfiguration;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAdminServer
-@SpringBootApplication
+@SpringBootApplication(exclude = AdminServerHazelcastAutoConfiguration.class)
 public class AdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
 }

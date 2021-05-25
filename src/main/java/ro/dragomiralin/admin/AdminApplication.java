250c79e8b19ca309e7060954e9b3f3a7cdc8d2e3
dragomiralin <dragomirdanielalin@yahoo.com>
2021-05-25 19:03:43 +0300

    add eureka client

--- src/main/java/ro/dragomiralin/admin/AdminApplication.java
+++ src/main/java/ro/dragomiralin/admin/AdminApplication.java
@@ -1,8 +1,11 @@
 package ro.dragomiralin.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableAdminServer
 @SpringBootApplication
 public class AdminApplication {
 

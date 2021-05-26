96f6d8cd37178459b600b14f702020ad1d386280
dragomiralin <dragomirdanielalin@yahoo.com>
2021-05-26 22:09:17 +0300

    downgrade version

--- src/main/java/ro/dragomiralin/admin/security/CustomDetailService.java
+++ src/main/java/ro/dragomiralin/admin/security/CustomDetailService.java
@@ -1,10 +1,3 @@
 package ro.dragomiralin.admin.security;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
